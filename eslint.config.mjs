fba4bb81bfed91fdd0ad73058f814120111b77ae
kpauner <kpauner@gmail.com>
2024-10-06 01:26:41 +0200

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import antfu from "@antfu/eslint-config";
+
+export default antfu(
+  {
+    type: "app",
+    typescript: true,
+    formatters: true,
+    stylistic: {
+      indent: 2,
+      semi: true,
+      quotes: "double",
+    },
+  },
+  {
+    rules: {
+      "no-console": ["warn"],
+      "antfu/no-top-level-await": ["off"],
+      "node/prefer-global/process": ["off"],
+      "perfectionist/sort-imports": [
+        "error",
+        {
+          internalPattern: ["@/**"],
+        },
+      ],
+      "unicorn/filename-case": [
+        "error",
+        {
+          case: "kebabCase",
+          ignore: ["README.md"],
+        },
+      ],
+    },
+  },
+);
